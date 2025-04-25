dcc93853c6a90634660d2b0b587142d3e75ef84c
mroatmeal <jfeind101@gmail.com>
2025-04-25 10:34:00 -0500

    finished test clean

--- online_retail_cleaning.py
+++ online_retail_cleaning.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd
+import csv
 from datetime import datetime
 
 df = pd.read_csv("unclean/test_online_retail.csv")
+
+# Making a copy of df to make sure the raw data is not modified. 
+df_clean = df.copy()
 
 # The beginning process of cleaning this dataset is to have a consistent column title
 # this includes removing spaces, capitilization, and non-descriptive titles. 
@@ -19,28 +23,56 @@ df = pd.read_csv("unclean/test_online_retail.csv")
 #     "Country":"country"}, inplace=True)
 
 # You can also automate the column titles if they are dynamic or if there are a lot of columns in the data set. 
-df.columns = df.columns.str.strip().str.lower().str.replace(' ','_')
+df_clean.columns = df_clean.columns.str.strip().str.lower().str.replace(' ','_')
 # Then rename specific ones afterwards. 
-df.rename(columns={    
+df_clean.rename(columns={    
     "invoice":"invoice_number",
     "stockcode":"stock_code",
     "invoicedate":"invoice_date"
     }, inplace=True)
 
-# print(df.columns)
+# print(df_clean.columns)
 
 # The next step is to review the column invoice_number. 
     # first step is to see if there are duplicated values
     # using the built in pandas method duplicated()
     # also using the subset argument of duplicated() to refine search as there could be duplicated Invoices with different dates. 
-# print(df[df.duplicated(subset=['invoice_number', 'stock_code', 'quantity', 'price'])])
+# print(df_clean[df_clean.duplicated(subset=['invoice_number', 'stock_code', 'quantity', 'price'])])
 
-# If you would like to log the duplicates you can 
-# if df.duplicated(subset=['invoice_number', 'stock_code']).any():
-#     print(df[df.duplicated()])
+# If you would like to log the duplicates you can also use an if statement with the built in .any() method
+    # .any() method checks if any of the values in the Bollean Series are True.  
+# if df_clean.duplicated(subset=['invoice_number', 'stock_code']).any():
+#     print(df_clean[df_clean.duplicated()])
 # else:
-#     # print("no duplicates")
+#     print("no duplicates")
 
-# After identifying that there are duplicates you want to drop the duplicates using 
-df_clean = df.drop_duplicates(subset=['invoice_number', 'stock_code'], keep='first')
-print(df_clean)
+# After identifying that there are duplicates you want to drop the duplicates using drop_duplicates()
+    # using subset arguments invoice number and stock code,
+df_clean = df_clean.drop_duplicates(subset=['invoice_number', 'stock_code'], keep='first')
+# print(df_clean)
+
+# Then after identifying/removing duplicates you want to check for missing or null values. 
+    # Can check for missing values using .isna() combined with aggregate function .sum()
+    # this will sum all of the Boolean values of True. 
+# print(df.isna().sum())
+# After seeing that there are missing values you can inspect the missing values again with .isna() and .any()
+# print(df[df.isna().any(axis=1)])
+# After inspection the next step is to replace the missing values with np.nan
+df_clean.replace(['NaN','NA','N/A','null',''], np.nan, inplace=True)
+# print(df_clean)
+# print(df[df.isna().any(axis=1)])
+# print(df_clean.info())
+
+# The next step is to correct the data types for each column for smooth importing into pandas.
+df_clean['invoice_number'] = df_clean['invoice_number'].astype(str)
+df_clean['stock_code'] = df_clean['stock_code'].astype(str)
+df_clean['description'] = df_clean['description'].astype(str)
+df_clean['quantity'] = pd.to_numeric(df_clean['quantity'], errors='coerce').astype('Int64')
+df_clean['invoice_date'] = pd.to_datetime(df_clean['invoice_date'], format='%Y-%m-%d %H:%M:%S')
+df_clean['price'] = pd.to_numeric(df_clean['price'], errors='coerce')
+df_clean['customer_id'] = pd.to_numeric(df_clean['customer_id'], errors='coerce').astype('Int64')
+df_clean['country'] = df_clean['country'].astype(str)
+
+# Last step is to export the cleaned data and wrap all the fields in double qutoes.
+    # this is to close off open quotes and extra commas in the data. 
+df_clean.to_csv('clean/cleaned_online_retail_test.csv', index=False, quoting=csv.QUOTE_ALL)
